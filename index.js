f442943a13149c29bd9919fb4be90fa94e6560cd
Tony Pujals <tony@subfuzion.com>
2014-02-27 16:49:38 -0800

    closes #1, closes #2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-module.exports {
+module.exports = {
   Server: require('./lib/server'),
   Client: require('./lib/client')
 };
-
--- test/test.js
+++ test/test.js
@@ -1,24 +1,70 @@
-var assert = require('assert');
+var assert = require('assert')
+  , echo = require('..')
+  , port = 5555
+  , uri = 'ws://localhost:' + port
+  ;
 
 var notImplementedError = new Error('not implemented yet');
 
 describe ('echo server test suite', function() {
 
   it ('should start on specified TCP port', function(done) {
-    done(notImplementedError);
+    var server = new echo.Server(port);
+    server.start(function(err) {
+      // release port for subsequent tests
+      if (!err) server.close();
+      done(err);
+    });
+  });
+
+  it ('should callback with an error when the address is in use', function(done) {
+    var server = new echo.Server(port);
+
+    server.start(function(err) {
+      if (err) return done(err);
+
+      // server started ok
+      // confirm that we can't start another server on the same port
+      var server2 = new echo.Server(port);
+      server2.start(function(err) {
+        // release port for subsequent tests
+        server.close();
+
+        // expected a callback error
+        assert.equal(err.code, 'EADDRINUSE');
+        done();
+      })
+    });
   });
 
   it ('should echo received messages', function(done) {
-    done(notImplementedError);
+    var message = 'hello';
+
+    var server = new echo.Server(port);
+    var client = new echo.Client(uri);
+
+    server.start(function(err) {
+      if (err) return done(err);
+
+      client.on('message', function(echo) {
+        assert.equal(echo, message);
+        done();
+      });
+
+      client.on('open', function() {
+        client.send(message);
+      });
+    });
+
   });
 
-  it ('should process HISTORY command to return message history', function(done) {
+  it.skip ('should process HISTORY command to return message history', function(done) {
     done(notImplementedError);
   });
 
 });
 
-describe ('echo client test suite', function() {
+describe.skip ('echo client test suite', function() {
 
   it ('should send messages on specified port', function(done) {
     done(notImplementedError);
